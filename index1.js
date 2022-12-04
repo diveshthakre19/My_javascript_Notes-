2efd4b0d972203d191da6a15d6b08278e1b96c33
diveshthakre19 <diveshthakre07@gmail.com>
2022-12-05 00:11:55 +0530

    main topics to cover

--- index1.js
+++ index1.js
@@ -1,1 +1,78 @@
 // Full javascript prepration notes by -Divesh thakre
+/*
+ Topics
+ 1) Variable and Data types
+ 2) const, let and var 
+ 3) Primitive and object
+ 4) Operators and Expressions 
+ 5) Conditionals 
+ 6) For Loop 
+ 7) While Loop 
+ 8) Do While Loop 
+ 9) Functions 
+ 10) Strings 
+ 11) Strings Methods
+ 12) Array 
+ 14) Array Methods 
+ 15) Using Loop with Arrays  
+ 16) Map
+ 17) Filter  
+ 18) Reduce 
+ 19) Js in Browser
+ 20) Js console object 
+ 21) Alert, Prompt and Confirm
+ 22) DOM, BOM and Window Object 
+ 23) DOM 
+ 24) Accessing children of an element 
+ 25) Parent and Sibling of an Element 
+ 26) Element only navigation 
+ 27) Table Navigation 
+ 28) Serching the Dom 
+ 29) Matches, Closest and Contains
+ 30) Inner HTML & Outer HTML
+ 31) HTML Attributes and Their Methods 
+ 32) HTML issertion method 
+ 33) insertAdjacentHTML etc
+ 34) className and classList etc
+ 35) setInterval and setTimeout 
+ 36) Browser Events 
+ 37) Handeling Browser Events 
+ 38) Callbacks
+ 39) Callback Hell & Pyramid
+ 40) Promises, .then() & .catch()
+ 41) Promises Chaining .then() calls 
+ 42) Attaching Multiple Handlers to a Promise
+ 43) The Promise API
+ 44) Async/Await 
+ 45) Error handeling try and catch 
+ 46) The Error Object and Custom Errors
+ 47) Clause
+ 48) Fetch API
+ 49) POST req with Fetch API
+ 50) Cookies 
+ 51) localStorage and related methods 
+ 52) session Storage and related methods 
+ 53) Object /oriented programing 
+ 54) Prototypes and _proto_ 
+ 55) Classes and objects
+ 56) Constructors 
+ 57) Static Method 
+ 58) getters, settors and instanceOf Operator 
+ 59) IIFE
+ 60) Destructuring and Spread operator 
+ 61) local and global scope
+ 62) Hosting 
+ 63) Closures
+ 64) Arrow Functions 
+ 65) JavaScript on the server
+ 66) npm and Node.js Packages
+ 67) Modules in js
+ 68) Intro to Express
+ 69) Regular Expression in js 
+ 70) Event loop in js
+
+
+
+
+*/
+// Every molecule of my energy will be spent to achive my goal for that i will go beyond plus ultra.
