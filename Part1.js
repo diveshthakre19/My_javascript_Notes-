8d6ef49fb58b74eae1bdbd01e14f0378dc471018
diveshthakre19 <diveshthakre07@gmail.com>
2022-12-05 00:43:36 +0530

    Basic topics

---
+++ Part1.js
@@ -0,0 +1,21 @@
+// 1) Variable and Data types
+// variables are like a like a Box Where we can store values.
+/* 
+ # Rules for choosing variables
+ - Letters, Digits, UnderScores and $ sing allowed
+ - Must begin with $, _ or a letter 
+ - js Reserved key words cannot be used as a variable name
+ - js is case sensitive language so be aware
+
+ # var vs let in Javascript 
+ - var is globaly Scoped  while let and const are block scoped.
+ - var can be updated and  redeclared  within its Scope.
+ - let can be updated but not redeclared.
+ - var variable are initalized with undefined where as let and const variables are not inatilized 
+ - const must be inatilized during declarition unlike let and var.
+*/
+let a = 85; // this a contains 85
+a = "Eighty seven";
+console.log(a); // "Eighty seven"
+
+// let 4divesh = "this is not allowed"
